c3bb6df1eb4e2d555789c1e0b0776cf4f25f8935
DESKTOP-N9C8HF0\Konstantin <trigger.kst@gmail.com>
2020-10-24 21:15:01 +0600

    показал как сделать только одну загрузку всех данных для карусели, а по клику на табы будем из массива брать нужные слайды

--- src/js/my.js
+++ src/js/my.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     /*карусель для модуля Новинки каталога на главной*/
-  const mySwiper = new Swiper('.module-mp-cat-news .swiper-container', {
+    const mySwiper = new Swiper('.module-mp-cat-news .swiper-container', {
         slidesPerView: 5,
         spaceBetween: 20,
         navigation: {
@@ -8,7 +8,15 @@ $(document).ready(function () {
             prevEl: '.module-mp-cat-news .swiper-button-prev',
         },
     });
-    /!*карусель для модуля Акционный товар на главной*!/
+
+    const actions_all_slides_array = []; //все слайды карусельки с акциями
+
+    if (actions_default_slides_array) {
+        for (const [i, arr] of actions_default_slides_array.entries()) {
+            actions_all_slides_array.push(arr['html'])
+        }
+    }
+    /*карусель для модуля Акционный товар на главной*/
     const mySwiper2 = new Swiper('.module-mp-cat-actions .swiper-container', {
         slidesPerView: 5,
         spaceBetween: 20,
@@ -17,6 +25,8 @@ $(document).ready(function () {
             prevEl: '.module-mp-cat-actions .swiper-button-prev',
         },
     });
+    mySwiper2[0].appendSlide(actions_all_slides_array);
+
     /!*карусель для модуля медиацентр на главной*!/
     const mySwiper3 = new Swiper('.module-mp-media-center .swiper-container', {
         slidesPerView: 3,
@@ -41,8 +51,8 @@ $(document).ready(function () {
     });
 
     /*кастом аккордеон для бокового меню*/
-    $(".accordion_tab").click(function(e){
-        if($(this).hasClass("active")){
+    $(".accordion_tab").click(function (e) {
+        if ($(this).hasClass("active")) {
             $(this).parent().removeClass("active");
             $(this).next().slideUp();
             $(this).removeClass("active");
@@ -56,14 +66,14 @@ $(document).ready(function () {
 
     /*обертка для скриптра кастом скролла*/
     /*http://gromo.github.io/jquery.scrollbar/*/
-    const myScroll = function() {
+    const myScroll = function () {
         $('.j-sidemenu-scrollbar-wrapper .scrollbar-inner').scrollbar({
             //autoScrollSize:!1,
             autoUpdate: true,
             disableBodyScroll: false
         });
     };
-    const myScroll2 = function() {
+    const myScroll2 = function () {
         $('.j-megamenu-scrollbar-wrapper .scrollbar-inner').scrollbar({
             //autoScrollSize:!1,
             autoUpdate: true,
@@ -76,7 +86,7 @@ $(document).ready(function () {
 
     /*Открытие / закрытие бокового меню*/
     $(".burger--btn").click(function () {
-        if($(this).hasClass('opened')){
+        if ($(this).hasClass('opened')) {
             $("body").removeClass("sidemenu--open");
             $("#sidemenu").removeClass("open");
             $(this).removeClass("opened");
@@ -100,10 +110,9 @@ $(document).ready(function () {
         $(".mm-popup").removeClass("active");
         $(this).addClass("active");
         let catId = $(this).attr("data-menu-id");
-        $(".megamenu[data-id="+catId+"]").addClass("active");
-        $(".mm-popup[data-itemid="+catId+"]").addClass("active");
+        $(".megamenu[data-id=" + catId + "]").addClass("active");
+        $(".mm-popup[data-itemid=" + catId + "]").addClass("active");
     });
-
 
 
     $(".minicat").hover(function () {
@@ -113,17 +122,17 @@ $(document).ready(function () {
         let imgHeight = $(this).find("img").height();
         let myFullHeight = $(this).find(".submenu").height() + imgHeight + nameHeight;
         let myOffset = $(this).position().top + myFullHeight;
-        let myOffset2 = $(this).position().top + $(this).height()-40;
+        let myOffset2 = $(this).position().top + $(this).height() - 40;
 
         let sub = myOffset - containerHeight;
 
-        if(myOffset2<containerHeight) {
+        if (myOffset2 < containerHeight) {
             $(this).addClass("hover");
             $(this).find(".submenu").css({"paddingTop": nameHeight + 130});
 
             if (myOffset > containerHeight) {
-                $(this).find(".submenu").css({"top": sub * (-1) -8 - 20});
-                $(this).find(">a").css("top", sub * (-1) -10);
+                $(this).find(".submenu").css({"top": sub * (-1) - 8 - 20});
+                $(this).find(">a").css("top", sub * (-1) - 10);
             }
         }
     }, function () {
@@ -132,23 +141,21 @@ $(document).ready(function () {
     });
 
 
-
-
     const basePopImgHeight = $(".mm--popular>ul>li img").height();
 
     $(".mm--popular>ul>li").hover(function () {
-        let ulHeight = $(this).find(".submenu ul").height();
-       // $(this).find(".submenu").css({"paddingTop":basePopImgHeight + 80 - ulHeight, "maxHeight":"auto"});
-        $(this).find(".submenu").css({"maxHeight":ulHeight+10});
-        $(this).find(".pic img").css({"height":basePopImgHeight-ulHeight+54});
-    },
-    function () {
-        $(this).find(".pic img").css({"height":basePopImgHeight});
-        $(this).find(".submenu").removeAttr("style");
-    }
+            let ulHeight = $(this).find(".submenu ul").height();
+            // $(this).find(".submenu").css({"paddingTop":basePopImgHeight + 80 - ulHeight, "maxHeight":"auto"});
+            $(this).find(".submenu").css({"maxHeight": ulHeight + 10});
+            $(this).find(".pic img").css({"height": basePopImgHeight - ulHeight + 54});
+        },
+        function () {
+            $(this).find(".pic img").css({"height": basePopImgHeight});
+            $(this).find(".submenu").removeAttr("style");
+        }
     );
 
-    $(".megamenu-inner").mouseleave(function(){
+    $(".megamenu-inner").mouseleave(function () {
         $(".megamenu").removeClass("active");
         $(".category_menu li").removeClass("active");
         $(".megamenu--catalog li:not(.viewall-margin)").removeClass("active");
@@ -169,37 +176,59 @@ $(document).ready(function () {
         $(".megamenu--catalog li:not(.viewall-margin)").removeClass("active");
         $(this).addClass("active");
         let catId = $(this).attr("data-id");
-        $(".mm-popup[data-itemId="+catId+"]").addClass("active");
+        $(".mm-popup[data-itemId=" + catId + "]").addClass("active");
     });
 
 
     $(".module_carousel--item").hover(function () {
         let addHeight = $(this).find(".additions").height();
-       $(this).find(".shover").css({"height": $(this).height()+addHeight*2+16});
-       $(this).css({"paddingBottom": addHeight*2});
+        $(this).find(".shover").css({"height": $(this).height() + addHeight * 2 + 16});
+        $(this).css({"paddingBottom": addHeight * 2});
     }, function () {
         $(this).find(".shover").removeAttr("style");
-        $(this).css({"paddingBottom":"20px"});
+        $(this).css({"paddingBottom": "20px"});
     });
-
-
 
 
     /*tabs*/
     $(".tabs li").on("click", function () {
         let tabId = $(this).parent().parent().attr("data-tabid");
-        let $tabs = $(".tabs_content_wrapper[data-tabcid="+tabId+"]");
+        let $tabs = $(".tabs_content_wrapper[data-tabcid=" + tabId + "]");
         let index = $(this).index();
         let swiperName = eval($(this).parent().parent().attr("data-swiper"));
+        let slug =  $(this).attr('data-slug').toString();
+
+
+        let temp_array = [];
+
+        if (actions_default_slides_array) {
+            if (slug === 'all'){
+                for (const [i, arr] of actions_default_slides_array.entries()) {
+                    temp_array.push(arr['html'])
+                }
+            } else {
+                for (const [i, arr] of actions_default_slides_array.entries()) {
+                    if(arr['slug'] === slug) {
+                        temp_array.push(arr['html'])
+                    }
+                }
+            }
+        }
+
+        console.log(slug);
+        console.log(temp_array);
+
         $(this).parent().parent().find("li").removeClass("active");
         $(this).addClass("active");
-        $tabs.find(".tabs_content").each(function () {
+       /* $tabs.find(".tabs_content").each(function () {
             $(this).removeClass('active');
         });
         $tabs.find(".tabs_content").eq(index).addClass("active");
-        $tabs.parent().find(".swiper-button-prev").addClass("swiper-button-disabled");
-        swiperName[index].update();
-        swiperName[index].slideTo(0,0);
+        $tabs.parent().find(".swiper-button-prev").addClass("swiper-button-disabled");*/
+        swiperName[0].removeAllSlides();
+        swiperName[0].appendSlide(temp_array);
+        swiperName[0].update();
+        swiperName[0].slideTo(0, 0);
     })
 
 
@@ -213,10 +242,10 @@ $(document).ready(function () {
     $(".card .btn-decrease").click(function (e) {
         e.preventDefault();
         let cur = parseInt($(this).parent().find("input").val());
-        if(cur > 0) {
+        if (cur > 0) {
             $(this).parent().find("input").val(cur - 1);
             $(this).parent().parent().find(".notify").text(cur - 1);
-            if((cur-1) == 0){
+            if ((cur - 1) == 0) {
                 $(this).parent().parent().removeClass("hasItems");
                 $(this).parent().parent().find(".notify").addClass("notify--empty").text("+");
             }
@@ -230,19 +259,18 @@ $(document).ready(function () {
         $(this).parent().parent().find(".notify").removeClass("notify--empty").text("1");
     })
 
-/* ToDo:
-*   1. Прокрутка вверх в левом меню при ховере на топ меню
-*   2. Слайдер на главной
-*   3. ТопТоп меню
-*
-* */
+    /* ToDo:
+    *   1. Прокрутка вверх в левом меню при ховере на топ меню
+    *   2. Слайдер на главной
+    *   3. ТопТоп меню
+    *
+    * */
 
 });
 
 
-
 //https://github.com/hsynlms/zeynepjs
-$(function() {
+$(function () {
     // loop all zeynepjs menus for initialization
     $('.zeynep').each(function () {
         // init zeynepjs side menu
